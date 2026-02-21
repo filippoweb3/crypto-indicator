113245a053dd57be3bb545be9e65999d45638dd8
filippoweb3 <flcf@protonmail.com>
2026-02-21 19:43:50 +0100

    minor edits

--- global_variables.R
+++
@@ -1,7 +0,0 @@
-global_variables <- list(
-  fred_api_key = "cf6226d9e99e3f8d775709800fc3576b",
-  cryptoQuant_api_key = "nlssEEeZ22Sqwef1Y8Raj1KJk0vHlnUW6gXcJFTt5MDvrenBHM4GZomyd4bswX",
-  whaleAlert_api_key = "qWwKzooOYtth6wXzTPBTEBWyuVDKwstA"
-)
-
-usethis::use_data(global_variables, overwrite = T) # save/overwrite global variables data
